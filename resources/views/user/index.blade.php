d3fa5b03ee29ab493ac8ee0431eb01f6bf580c76
Rodrigo González <rodrigo99_07_13@hotmail.com>
2024-10-07 21:57:55 -0500

    update

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -13,17 +13,14 @@
                     </div>
                     <div class="container mb-3">
                         <!-- Search -->
-                    <div class="navbar-nav">
-                        <div class="nav-item d-flex align-items-center">
-                            <i class="bx bx-search fs-4 lh-0"></i>
-                            <input
-                                type="text"
-                                class="form-control border-0 shadow-none"
-                                placeholder="Buscar..."
-                                aria-label="Search..." />
+                        <div class="navbar-nav">
+                            <div class="nav-item d-flex align-items-center">
+                                <i class="bx bx-search fs-4 lh-0"></i>
+                                <input type="text" class="form-control border-0 shadow-none" placeholder="Buscar..."
+                                    aria-label="Search..." />
+                            </div>
                         </div>
-                    </div>
-                    <!-- /Search -->
+                        <!-- /Search -->
                     </div>
 
                     @if ($message = Session::get('success'))
@@ -35,90 +32,88 @@
                     <div class="table-responsive text-nowrap flex-grow-1">
                         <table class="table table-striped">
                             <thead>
+                                <tr>
+                                    <th>No</th>
+
+                                    <th>Nombres</th>
+                                    <th>Apellidos</th>
+                                    <th>Telefono</th>
+                                    <th>Cedula</th>
+                                    <th>Ciudad</th>
+                                    <th>Departamento</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($users as $user)
                                     <tr>
-                                        <th>No</th>
-
-                                        <th>Nombres</th>
-                                        <th>Apellidos</th>
-                                        <th>Telefono</th>
-                                        <th>Cedula</th>
-                                        <th>Ciudad</th>
-                                        <th>Departamento</th>
-                                        <th>Acciones</th>
+                                        <td>{{ ++$i }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->apellidos }}</td>
+                                        <td>{{ $user->telefono }}</td>
+                                        <td>{{ $user->cedula }}</td>
+                                        <td>{{ $user->ciudad }}</td>
+                                        <td>{{ $user->departamento }}</td>
+                                        <td>
+                                            <div class="dropdown">
+                                                <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+                                                    <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
+                                                        data-bs-toggle="dropdown">
+                                                        <i class="bx bx-dots-vertical-rounded"></i>
+                                                    </button>
+                                                    <div class="dropdown-menu">
+                                                        <a class="dropdown-item"
+                                                            href="{{ route('users.show', $user->id) }}">
+                                                            <i class="bx bx-show-alt me-1"></i> Ver
+                                                        </a>
+                                                        <a class="dropdown-item"
+                                                            href="{{ route('users.edit', $user->id) }}">
+                                                            <i class="bx bx-edit-alt me-1"></i> Editar
+                                                        </a>
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <a class="dropdown-item" href="javascript:void(0);"
+                                                            onclick="event.preventDefault(); showDeleteModal({{ $user->id }});">
+                                                            <i class="bx bx-trash me-1"></i> Eliminar
+                                                        </a>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($users as $user)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
-                                            <td>{{ $user->name }}</td>
-                                            <td>{{ $user->apellidos }}</td>
-                                            <td>{{ $user->telefono }}</td>
-                                            <td>{{ $user->cedula }}</td>
-                                            <td>{{ $user->ciudad }}</td>
-                                            <td>{{ $user->departamento }}</td>
-                                            <td>
-                                                <div class="dropdown">
-                                                    <form
-                                                        action="{{ route('users.destroy', $user->id) }}"
-                                                        method="POST">
-                                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
-                                                            data-bs-toggle="dropdown">
-                                                            <i class="bx bx-dots-vertical-rounded"></i>
-                                                        </button>
-                                                        <div class="dropdown-menu">
-                                                            <a class="dropdown-item"
-                                                                href="{{ route('users.show', $user->id) }}">
-                                                                <i class="bx bx-show-alt me-1"></i> Ver
-                                                            </a>
-                                                            <a class="dropdown-item"
-                                                                href="{{ route('users.edit', $user->id) }}">
-                                                                <i class="bx bx-edit-alt me-1"></i> Editar
-                                                            </a>
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            <a class="dropdown-item" href="javascript:void(0);"
-                                                                onclick="event.preventDefault(); showDeleteModal({{ $user->id }});">
-                                                                <i class="bx bx-trash me-1"></i> Eliminar
-                                                            </a>
-                                                        </div>
-                                                    </form>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-                {!! $users->withQueryString()->links() !!}
-
-                <!-- Modal -->
-                <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel"
-                    aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="deleteModalLabel">Confirmación de eliminación</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                ¿Estás seguro de que quieres eliminar este registro?
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary"
-                                    data-bs-dismiss="modal">Cancelar</button>
-                                <button type="button" class="btn btn-danger" id="confirmDelete">Eliminar</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
             </div>
+            {!! $users->withQueryString()->links() !!}
+
+            <!-- Modal -->
+            <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="deleteModalLabel">Confirmación de eliminación</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ¿Estás seguro de que quieres eliminar este registro?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            <button type="button" class="btn btn-danger" id="confirmDelete">Eliminar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
+
+
 
     <script>
         let deleteFormAction;
