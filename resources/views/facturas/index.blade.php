012ee8c2e52a5ea202b24afab34ba41ff50c2cd2
Rodrigo González <rodrigo99_07_13@hotmail.com>
2024-10-07 21:51:58 -0500

    update

--- resources/views/facturas/index.blade.php
+++ resources/views/facturas/index.blade.php
@@ -17,6 +17,20 @@
                         </div>
                     @endif
 
+                    <div class="container mb-3">
+                        <!-- Search -->
+                    <div class="navbar-nav">
+                        <div class="nav-item d-flex align-items-center">
+                            <i class="bx bx-search fs-4 lh-0"></i>
+                            <input
+                                type="text"
+                                class="form-control border-0 shadow-none"
+                                placeholder="Buscar..."
+                                aria-label="Search..." />
+                        </div>
+                    </div>
+                    <!-- /Search -->
+
                     <div class="table-responsive text-nowrap flex-grow-1">
                         <table class="table table-striped">
                             <thead>
--- resources/views/tarjetapro/index.blade.php
+++ resources/views/tarjetapro/index.blade.php
@@ -12,6 +12,20 @@
                         </div>
                     </div>
 
+                    <div class="container mb-3">
+                        <!-- Search -->
+                    <div class="navbar-nav">
+                        <div class="nav-item d-flex align-items-center">
+                            <i class="bx bx-search fs-4 lh-0"></i>
+                            <input
+                                type="text"
+                                class="form-control border-0 shadow-none"
+                                placeholder="Buscar..."
+                                aria-label="Search..." />
+                        </div>
+                    </div>
+                    <!-- /Search -->
+
                     @if ($message = Session::get('success'))
                         <div class="alert alert-success m-4">
                             <p>{{ $message }}</p>
