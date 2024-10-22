8b0403da37126622e9b9bf16b3d417fc61b32582
Rodrigo González <rodrigo99_07_13@hotmail.com>
2024-10-22 16:33:57 -0500

    update

--- vendor/laravel/framework/src/Illuminate/Notifications/resources/views/email.blade.php
+++ vendor/laravel/framework/src/Illuminate/Notifications/resources/views/email.blade.php
@@ -6,7 +6,7 @@
 @if ($level === 'error')
 # @lang('Whoops!')
 @else
-# @lang('Hello!')
+# @lang('Hola!')
 @endif
 @endif
 
@@ -47,8 +47,8 @@
 @isset($actionText)
 <x-slot:subcopy>
 @lang(
-    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser:',
+    "Si tiene problemas para hacer clic en el botón \":actionText\", Copia y pega la URL a continuación\n".
+    'en su navegador web:',
     [
         'actionText' => $actionText,
     ]
