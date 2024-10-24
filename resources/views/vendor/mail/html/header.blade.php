382e72aa3546d50c618a29f5c895c5356d068ba0
Rodrigo González <rodrigo99_07_13@hotmail.com>
2024-10-23 21:51:30 -0500

    update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
+@if (trim($slot) === 'Servigas del Huila')
 <img src="{{ asset('build/assets/dashboard/assets/img/favicon/favicon.ico') }}" class="logo" alt="Laravel Logo">
 <h1 class="mt-3">Servigas del Huila</h1>
 @else
